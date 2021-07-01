ea953992056a5c7af1e83d5c742dee9312b52216
Romain Cambone <79193092+Romain056@users.noreply.github.com>
2021-07-01 13:47:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import matplotlib as plt
 import streamlit as st
 import streamlit.components.v1 as components
 from PIL import Image
-export STREAMLIT_SERVER_PORT=80
 
 
 st.set_page_config(layout="wide")
